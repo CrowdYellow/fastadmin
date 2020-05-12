5f749724ff2ebbe3b0722fa650374ef676c6b66b
CrowdYellow <echo61546@gmail.com>
2020-05-12 18:03:25 +0800

    v1.0.16 活动页面配置按钮的显示

--- public/assets/js/backend/activity.js
+++ public/assets/js/backend/activity.js
@@ -28,26 +28,100 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'admin.username', title: __('Admin')},
                         {field: 'festival.name', title: __('Festival')},
                         {field: 'title', title: __('Title')},
-                        {field: 'is_set_blade', title: __('Is_set_blade'), formatter:function(value,row){
-                                if (row.is_set_blade === 1)
+                        {
+                            field: 'is_set_prize',
+                            width: "120px",
+                            title: __('Is_set_prize'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            buttons: [
                                 {
-                                    return '<span style="color:green">已完成</span>';
-                                }else if (row.admin_id == Config.admin.id){
-                                    return '<span style="color:red">前往设置</span>';
-                                } else {
-                                    return '<span style="color:red">未完成</span>';
+                                    name: 'prize_finish',
+                                    text: __('已完成'),
+                                    title: __('已完成'),
+                                    classname: 'text-success'
+                                },
+                                {
+                                    name: 'set_prize',
+                                    text: '设置奖品',
+                                    title: '设置奖品',
+                                    classname: 'text-danger btn-dialog',
+                                    url: '/EayIOJxtvB.php/activity/setprize'
+                                },
+                                {
+                                    name: 'prize',
+                                    text: '未完成',
+                                    title: '未完成',
+                                    classname: 'text-danger'
                                 }
+                            ],
+                            formatter: function(value, row, index) {
+                                var that = $.extend({}, this);
+                                var table = $(that.table).clone(true);
+                                //判断什么时候显示什么时候不显示
+                                $(table).data("operate-del", null);
+                                $(table).data("operate-edit", null);
+                                if (row.Is_set_prize === 1) // 显示已完成
+                                {
+                                    $(table).data("operate-set_prize", null);
+                                    $(table).data("operate-prize", null);
+                                }else if (row.admin_id == Config.admin.id){ // 显示前往设置
+                                    $(table).data("operate-prize_finish", null);
+                                    $(table).data("operate-prize", null);
+                                } else {
+                                    $(table).data("operate-set_prize", null);
+                                    $(table).data("operate-prize_finish", null);
+                                }
+                                that.table = table;
+                                return Table.api.formatter.operate.call(that, value, row, index);
                             }
                         },
-                        {field: 'is_set_prize', title: __('Is_set_prize'), formatter:function(value,row){
-                                if (row.is_set_prize === 1)
+                        {
+                            field: 'is_set_blade',
+                            width: "120px",
+                            title: __('Is_set_blade'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            buttons: [
                                 {
-                                    return '<span style="color:green">已完成</span>';
-                                }else if (row.admin_id == Config.admin.id){
-                                    return '<span style="color:red">前往设置</span>';
-                                } else {
-                                    return '<span style="color:red">未完成</span>';
+                                    name: 'blade_finish',
+                                    text: __('已完成'),
+                                    title: __('已完成'),
+                                    classname: 'text-success'
+                                },
+                                {
+                                    name: 'set_blade',
+                                    text: '设置模板',
+                                    title: '设置模板',
+                                    classname: 'text-danger btn-dialog',
+                                    url: '/EayIOJxtvB.php/activity/setprize'
+                                },
+                                {
+                                    name: 'blade',
+                                    text: '未完成',
+                                    title: '未完成',
+                                    classname: 'text-danger'
                                 }
+                            ],
+                            formatter: function(value, row, index) {
+                                var that = $.extend({}, this);
+                                var table = $(that.table).clone(true);
+                                //判断什么时候显示什么时候不显示
+                                $(table).data("operate-del", null);
+                                $(table).data("operate-edit", null);
+                                if (row.Is_set_prize === 1) // 显示已完成
+                                {
+                                    $(table).data("operate-set_blade", null);
+                                    $(table).data("operate-blade", null);
+                                }else if (row.admin_id == Config.admin.id){ // 显示前往设置
+                                    $(table).data("operate-blade_finish", null);
+                                    $(table).data("operate-blade", null);
+                                } else {
+                                    $(table).data("operate-set_blade", null);
+                                    $(table).data("operate-blade_finish", null);
+                                }
+                                that.table = table;
+                                return Table.api.formatter.operate.call(that, value, row, index);
                             }
                         },
                         {field: 'start_at', title: __('Start_at')},
@@ -67,15 +141,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                             title: __('Operate'),
                             table: table,
                             events: Table.api.events.operate,
-                            buttons: [
-                                {
-                                    name: 'prize',
-                                    text: '设置奖品',
-                                    title: '设置奖品',
-                                    classname: 'btn btn-xs btn-primary btn-dialog',
-                                    url: '/EayIOJxtvB.php/activity/setprize'
-                                }
-                            ],
                             formatter: function(value, row, index) {
                                 var that = $.extend({}, this);
                                 var table = $(that.table).clone(true);
@@ -83,7 +148,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                                 if (row.admin_id !== Config.admin.id) {
                                     $(table).data("operate-del", null);
                                     $(table).data("operate-edit", null);
-                                    $(table).data("operate-prize", null);
                                 }
                                 that.table = table;
                                 return Table.api.formatter.operate.call(that, value, row, index);

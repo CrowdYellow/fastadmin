ae978964df117f3ad56348e4b6814dc9cabe4c12
CrowdYellow <echo61546@gmail.com>
2020-05-12 10:14:24 +0800

    v1.0.6 抽奖活动

---
+++ application/admin/lang/zh-cn/activity.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Admin_id'     => '管理员ID',
+    'Festival_id'  => '节日ID',
+    'Title'        => '活动标题',
+    'Is_set_blade' => '设置模板',
+    'Is_set_prize' => '设置奖品',
+    'Start_at'     => '开启时间',
+    'End_at'       => '结束时间'
+];
